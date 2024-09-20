5796494099c45e6e467ec19a1e4cdbef2c76405d
g <gabep@hey.com>
2024-09-20 10:31:15 -0700

    new47

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,9 @@ function Nav() {
     <nav className="flex justify-center">
       <div className='grid grid-cols-1 md:grid-cols-3 w-full max-w-screen-xl'>
         <Link to='/' className="nav-link">Home</Link>
-{/*         
-        <Link to='/new' className="nav-link">NEW TRIBE</Link> */}
-        <Link to='/tribes' className="nav-link">Tribes</Link>
+        
+        <Link to='/new' className="nav-link">NEW TRIBE</Link>
+        {/* <Link to='/tribes' className="nav-link">Tribes</Link> */}
         <Link to='/contestants' className="nav-link">Contestants</Link>
       </div>
     </nav>
