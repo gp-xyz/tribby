9490330b184f3208ad1a81d444cb8fd42bf2bd3f
g <gabep@hey.com>
2024-09-20 10:29:19 -0700

    home47

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,17 +17,17 @@ function Home() {
         ))}
       </ul> */}
       
-      <div className='sofaheader lilbubble'> Registration CLOSED for Survivor 46, starting Feb 28, 2024</div>
+      <div className='sofaheader lilbubble'> Registration OPEN for Survivor 47 until Sept 25th</div>
 
 
       
 
       <div className='lilbubble pl-6 ml-6'>
-      <div className='pb-5'>Who will win Survivor 46? (first episode debuts <span className='text-slate-600'>Feb 28, 2024</span>)</div>
+      <div className='pb-5'>Who will win Survivor 47? (first episode debuts <span className='text-slate-600'>Sept 18, 2024</span>)</div>
       <div className='font-semibold'>Instructions</div>
         <div className='pl-2 pb-2'>*Choose a name, a vibe(words or emoji), and your 3 favorite contestants on the TV show. This is your Tribe.</div>
         <div className='pl-2 pb-2'>*Entry fee is $100. All entry fees will be awarded to the winning tribe. If multiple tribes pick the winner, the prize pool is divided proportionally. Payment must be made outside of this website somehow.</div>
-        <div className='pl-2 pb-2'>*The deadline to submit entries is when episode #2 of the show is aired, the evening of <span className='text-slate-600'>March 6</span>.</div>
+        <div className='pl-2 pb-2'>*The deadline to submit entries is when episode #2 of the show is aired, the evening of <span className='text-slate-600'>Sept 25th</span>.</div>
         <div className='pl-2 pb-2'>*If you need to modify your entry before the deadline, just make a new tribe. I will manually delete 'unwanted' tribes based on which entries are paid for.</div>
       </div>
 
